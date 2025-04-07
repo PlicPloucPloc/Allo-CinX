c512faf77adf2cd4fb899f4dedd70467f4c30894
romain <rdoulaud@gmail.com>
2025-04-07 13:00:15 +0200

    update: remove viewMode

--- allo-cinx/src/app/home/movie/movie.component.ts
+++ allo-cinx/src/app/home/movie/movie.component.ts
@@ -1,16 +1,15 @@
 import { Component, Input } from '@angular/core';
 import { Movie } from '../../models/movies';
-import { DatePipe, NgClass, NgStyle } from '@angular/common';
+import { DatePipe } from '@angular/common';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-movie',
   standalone: true,
-  imports: [NgClass, NgStyle, RouterLink, DatePipe],
+  imports: [RouterLink, DatePipe],
   templateUrl: './movie.component.html',
   styleUrl: './movie.component.scss'
 })
 export class MovieComponent {
   @Input({ required: true }) movie!: Movie;
-  @Input() viewMode: 'grid' | 'list' = 'grid';
 }
