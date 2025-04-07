c8bb500d8951fd8b84f366430b317c3933036e7f
romain <rdoulaud@gmail.com>
2025-04-07 11:33:41 +0200

    update: navbar

--- allo-cinx/src/app/home/home.component.ts
+++ allo-cinx/src/app/home/home.component.ts
@@ -1,5 +1,5 @@
-import { Component, inject, Input, OnInit } from '@angular/core';
-import { DatePipe, AsyncPipe, NgClass } from '@angular/common';
+import { Component, inject, OnInit } from '@angular/core';
+import { AsyncPipe, NgClass } from '@angular/common';
 import { MoviesService } from '../services/movies.service';
 import { BehaviorSubject, combineLatest, map, Observable } from 'rxjs';
 import { Movie } from '../models/movies';
@@ -8,55 +8,49 @@ import { MovieComponent } from './movie/movie.component';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [DatePipe, AsyncPipe, MovieComponent, NgClass
-  ],
+  imports: [AsyncPipe, MovieComponent, NgClass],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
-export class HomeComponent implements OnInit{
-  private readonly moviesService = inject(MoviesService)
-  movies$: Observable<Movie[]> = this.moviesService.getMovies()
+export class HomeComponent implements OnInit {
+  private readonly moviesService = inject(MoviesService);
+  movies$: Observable<Movie[]> = this.moviesService.getMovies();
   //filteredMovies$: Observable<Movie[]>;
   isGridView = true;
   searchTerm$ = new BehaviorSubject<string>('');
   sortCriteria$ = new BehaviorSubject<string>('title');
   filteredMovies$!: Observable<Movie[]>;
+
   ngOnInit() {
     this.movies$ = this.moviesService.getMovies();
-    
-    this.filteredMovies$ = combineLatest([
-      this.movies$,
-      this.searchTerm$,
-      this.sortCriteria$
-    ]).pipe(
+
+    this.filteredMovies$ = combineLatest([this.movies$, this.searchTerm$, this.sortCriteria$]).pipe(
       map(([movies, searchTerm, sortCriteria]) => {
         // First filter by search term
         let filtered = movies;
         if (searchTerm.trim() !== '') {
           const term = searchTerm.toLowerCase();
-          filtered = movies.filter(movie => 
-            movie.title.toLowerCase().includes(term) || 
-            movie.synopsis.toLowerCase().includes(term)
-          );
+          filtered = movies.filter((movie) => movie.title.toLowerCase().includes(term) || movie.synopsis.toLowerCase().includes(term));
         }
-        
+
         // Then sort
         return this.sortMoviesList(filtered, sortCriteria);
-      })
+      }),
     );
   }
+
   searchMovies(term: string) {
     this.searchTerm$.next(term);
   }
-  
+
   sortMovies(event: any) {
     this.sortCriteria$.next(event.target.value);
   }
-  
+
   toggleView() {
     this.isGridView = !this.isGridView;
   }
-  
+
   private sortMoviesList(movies: Movie[], criteria: string): Movie[] {
     switch (criteria) {
       case 'title':
